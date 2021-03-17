fe308d8b582c92f5f4a4a70c7bfa29e5a349c57a
erikbozic <erik@sowalabs.com>
2021-03-17 08:53:04 +0100

    Remove ReadLine, minimize allocation

--- csharp/optimized/Program.cs
+++ csharp/optimized/Program.cs
@@ -6,26 +6,62 @@ using System.Runtime.CompilerServices;
 
 var wordCount = new Dictionary<string, StrongBox<int>>(StringComparer.OrdinalIgnoreCase);
 
-using var stream = Console.OpenStandardInput();
-using var streamReader = new StreamReader(stream, System.Text.Encoding.UTF8, false, 64 * 1024);
+using var streamReader = new StreamReader(Console.OpenStandardInput()); // File.OpenText("/home/erik/src/sandbox/countwords/kjvbible_x10.txt"); // Console.OpenStandardInput();
+// using var streamReader = new StreamReader(stream, System.Text.Encoding.UTF8, false, 64 * 1024);
+const int bufferLength = 24 * 1024;
+char[] buffer = new char[bufferLength]; // char[] instead of span to avoid cost while indexing into it in the main loop
+string leftover = null;
+var concat = false;
 
-string line;
-while ((line = streamReader.ReadLine()) != null)
+while (streamReader.ReadBlock(buffer) > 0)
 {
-    int lastPos = 0;
-    for (int index = 0; index <= line.Length; index++)
+    // prepend leftover if any and new chunk starts with whitespace
+    if (leftover != null)
     {
-        if (index == line.Length || line[index] == ' ')
+        if (buffer[0] <= ' ')
+        {
+            if (!wordCount.TryGetValue(leftover, out var count)) {
+                wordCount.Add(leftover, new StrongBox<int>(1));
+            } else {
+                ++count.Value;
+            }
+            leftover = null;
+        }
+        concat = true;
+    }
+
+    int lastPos = 0;
+    for (int index = 0; index <= bufferLength; index++)
+    {
+        var endOfBuffer = index == bufferLength;
+        if (endOfBuffer || buffer[index] <= ' ')
         {
             if (lastPos < index)
             {
-                // Substring takes ~15% of the time
-                var word = line.Substring(lastPos, index - lastPos);
-                // TryGetValue takes ~55% of the time
-                if (!wordCount.TryGetValue(word, out var count)) {
-                    wordCount.Add(word, new StrongBox<int>(1));
-                } else {
-                    ++count.Value;
+                // Save the leftovers for next streamReader.Read() iteration
+                if (endOfBuffer)
+                {
+                    leftover = new String(buffer, lastPos, index - lastPos);
+                }
+                else
+                {
+                    string word;
+                    if (concat)
+                    {
+                        word = String.Concat(leftover, buffer.AsSpan(lastPos, index - lastPos));
+                        leftover = null;
+                        concat = false;
+                    }
+                    else
+                    {
+                        word = new String(buffer, lastPos, index - lastPos);
+                    }
+                    // TryGetValue takes ~65% of the time
+                    if (!wordCount.TryGetValue(word, out var count)) {
+                        wordCount.Add(word, new StrongBox<int>(1));
+                    } else {
+                        ++count.Value;
+                    }
                 }
             }
             lastPos = index + 1;
